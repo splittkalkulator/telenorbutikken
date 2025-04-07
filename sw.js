5bb6f3bfb532ddb59d67dd6c9481caf7fd3e67da
splittkalkulator <henrik.stoa.ingebo@telenor.no>
2025-04-07 09:39:39 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,27 +1,18 @@
-const CACHE_NAME = 'splitt-kalkulator-v1';
-const urlsToCache = [
-  '/telenorbutikken/',
-  '/telenorbutikken/index.html',
-  '/telenorbutikken/style.css', // Replace with your actual CSS file name
-  '/telenorbutikken/script.js'  // Replace with your actual JS file name
-];
-
-// Install the service worker and cache assets
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open('splitt-v1').then((cache) => {
+      return cache.addAll([
+        '/telenorbutikken/',
+        '/telenorbutikken/index.html'
+      ]);
+    })
   );
 });
 
-// Serve cached content when offline
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        return response || fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
